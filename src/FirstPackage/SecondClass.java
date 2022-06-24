a5565b2b42e86f1c5d659d1f4d443abe7f1a2c5c
MrGant <ciscogts@gmail.com>
2022-06-23 22:31:05 -0400

    I am confident on line 7

--- src/FirstPackage/SecondClass.java
+++ src/FirstPackage/SecondClass.java
@@ -3,5 +3,6 @@ package FirstPackage;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Hi its my first time here");
+        System.out.println("I feel confident now");
     }
 }
