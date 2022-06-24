47f9d807a7d35b9efc1519bc32fa807477cc5b70
MrGant <ciscogts@gmail.com>
2022-06-23 22:39:51 -0400

    my two test

---
+++ src/FirstPackage/myTwoTests.java
@@ -0,0 +1,9 @@
+package FirstPackage;
+
+public class myTwoTests {
+    public static void main(String[] args) {
+        System.out.println("First test case");
+        System.out.println("Second test case");
+
+    }
+}
