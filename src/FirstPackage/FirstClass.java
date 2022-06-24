8f17d46cbc9e773fd00b2a0e7cc0e7957f83605f
MrGant <ciscogts@gmail.com>
2022-06-23 22:22:13 -0400

    added three statemnets on line 6,7,8

---
+++ src/FirstPackage/FirstClass.java
@@ -0,0 +1,11 @@
+package FirstPackage;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello how are you doing");
+        System.out.println("Im fine , thank you");
+        System.out.println("And you?");
+    }
+
+}
